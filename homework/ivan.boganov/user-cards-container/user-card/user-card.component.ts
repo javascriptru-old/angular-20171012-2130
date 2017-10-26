c980c7c0b552fefaef6d95bfdb9e4c1d4f932e0f
Ivan Boganov <ivan.boganov@gmail.com>
2017-10-26 16:15:40 +0300

    Lesson 2 homework is done.

--- homework/ivan.boganov/user-cards-container/user-card/user-card.component.ts
+++ homework/ivan.boganov/user-cards-container/user-card/user-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { UserCard } from '../models/user-card';
+import { UserCard } from '../../models/user-card';
 
 @Component({
   selector: 'app-user-card',
