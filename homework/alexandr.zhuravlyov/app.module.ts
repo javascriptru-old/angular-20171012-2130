265a060bbf57d5336306b864c2d1dd412bb568a6
zhuravlyov <zhuravlyov@gmail.com>
2017-10-26 22:32:16 +0300

    Add files via upload

---
+++ homework/alexandr.zhuravlyov/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserService } from './user.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
